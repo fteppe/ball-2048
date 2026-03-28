7f618ac1fe806d55de5bde16060ebed0898f0d15
florent.teppe <teppe.florent@hotmail.fr>
2026-03-28 22:07:24 +0100

    improvements to spawner controls

--- ball_spawner.gd
+++ ball_spawner.gd
@@ -1,20 +1,39 @@
 extends Node2D
+class_name BallSpawner
 
-			
 var ball_scene = load("res://Ball.tscn")
-
+var is_held : bool = false
+var last_dropped_ball : Ball
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	generate_ball()
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func generate_ball():
+	if last_dropped_ball:
+		last_dropped_ball.body_entered.disconnect(ball_collided)
+	last_dropped_ball = ball_scene.instantiate()
+	self.add_child(last_dropped_ball)
+	last_dropped_ball.process_mode = Node.PROCESS_MODE_DISABLED
+
+func ball_collided(body):
+	generate_ball()
+	
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.pressed && event.button_index == 1:
-			var ball : Ball = ball_scene.instantiate()
-			ball.global_position = $SpawnPoint.global_position
-			self.get_parent().add_child(ball)
+			is_held = true
+		if event.is_released() && event.button_index == 1 && is_held:
+			is_held = false
+			last_dropped_ball.body_entered.connect(ball_collided)
+			last_dropped_ball.process_mode = Node.PROCESS_MODE_INHERIT
+			self.remove_child(last_dropped_ball)
+			last_dropped_ball.global_position = $SpawnPoint.global_position
+			self.get_parent().add_child(last_dropped_ball)
+
+func get_is_held():
+	return is_held
--- main_map.gd
+++ main_map.gd
@@ -9,7 +9,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
+var previous_mouse_pos_x : float
+
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		$BallSpawner.global_position.x = clampf($BallSpawner.get_global_mouse_position().x, $Left.global_position.x,  $Right.global_position.x) 
+	var current_mouse_pos = $BallSpawner.get_global_mouse_position().x
+	if event is InputEventMouseMotion && $BallSpawner.get_is_held():
+		$BallSpawner.global_position.x = clampf($BallSpawner.global_position.x + current_mouse_pos - previous_mouse_pos_x, $Left.global_position.x,  $Right.global_position.x)
+	
+	previous_mouse_pos_x = current_mouse_pos
