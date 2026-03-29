ad79689cc43c2bc3937231425e581e226e643d27
florent.teppe <teppe.florent@hotmail.fr>
2026-03-29 18:04:45 +0200

    ball generation rank improvement
    Still needs work to feel nice

--- ball_spawner.gd
+++ ball_spawner.gd
@@ -5,16 +5,27 @@ var ball_scene = load("res://Ball.tscn")
 var is_held : bool = false
 var last_dropped_ball : Ball
 var held_ball : Ball
+var max_rank_reached : int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	generate_ball()
+	GameModeBall.ball_created.connect(ball_created)
 	pass # Replace with function body.
+
+func ball_created(ball : Ball):
+	max_rank_reached = max(max_rank_reached, ball.rank)
+
+func get_rank_to_generate():
+	var max_rank_to_generate = max(max_rank_reached/2, 3)
+	var rank_to_generate = clampf(randfn(max_rank_to_generate / 2, 1), 1, max_rank_to_generate)
+	return rank_to_generate
 
 func generate_ball():
 	if last_dropped_ball:
 		last_dropped_ball.body_entered.disconnect(ball_collided)
 	last_dropped_ball = null
 	held_ball = ball_scene.instantiate()
+	held_ball.rank = get_rank_to_generate()
 	self.call_deferred("add_child", held_ball)
 	held_ball.process_mode = Node.PROCESS_MODE_DISABLED
 
