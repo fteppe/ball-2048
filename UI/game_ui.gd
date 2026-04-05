a4869fbc524ca21c26ea30cd3a37daa98a0efb1a
florent.teppe <teppe.florent@hotmail.fr>
2026-04-05 15:46:10 +0200

    score systems and various improvements to gamefeel

--- UI/game_ui.gd
+++ UI/game_ui.gd
@@ -3,13 +3,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GameModeBall.level_up.connect(_on_level_up)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	%Score.text = str(GameModeBall.get_score())
+	%ScoreProgress.value = GameModeBall.get_score()
 	pass
 
+func _on_level_up(new_level : int):
+	%ScoreProgress.max_value = GameModeBall.get_score_to_reach()
+	%CurrentLevel.text = str(GameModeBall.get_level())
+	$VBoxContainer/HBoxContainer/CurrentLevel/CPUParticles2D.restart()
 
 func _on_button_button_up():
 	get_tree().change_scene_to_file("res://MainMap.tscn")
--- game_mode_ball.gd
+++ game_mode_ball.gd
@@ -2,11 +2,25 @@ extends Node
 
 signal ball_created(ball : Ball)
 signal ball_died(ball: Ball)
+signal level_up(new_level : int)
+
+@export var level_scores : Array[int]
+
+var current_score : int = 0
+var current_level : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func get_level():
+	return current_level
+
+func get_score():
+	return current_score
+
+func get_score_to_reach():
+	return level_scores[min(level_scores.size() - 1, current_level)]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,5 +30,18 @@ func get_death_time() -> float:
 	return 5.
 
 func reset_game_mode():
+	current_score = 0
+	current_level = 0
 	print("game mode reset")
 	#do nothing for now
+
+func _level_up():
+	current_score = max(0 , current_score - get_score_to_reach())
+	current_level += 1
+	level_up.emit(current_level)
+
+func _on_ball_created(ball : Ball):
+	current_score += pow(2, ball.rank)
+	while current_score > get_score_to_reach():
+		_level_up()
+	pass # Replace with function body.
